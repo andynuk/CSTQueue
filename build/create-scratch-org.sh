599376b73d2e5b03077c093ae8414197c120cc69
Andy Nix <andynuk@gmail.com>
2019-07-27 14:26:49 +0100

    testing

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -3,18 +3,24 @@ echo "Setting up DevHub Connection..."
 mkdir keys
 echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
+TESTVAR=testing
+echo ${TESTVAR}
+
 # Authenticate to salesforce
 echo "Authenticating..."
-echo (sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub --json | jq --raw-output .result.orgId) | SFDC_AUTHENTICATE
-echo "Result..."
-echo $SFDC_AUTHENTICATE
-#echo "Reading OrgID..."
-#SFDC_AUTHENTICATE_ID = $(echo $SFDC_AUTHENTICATE | jq --raw-output .result.orgId)
-#echo $SFDC_AUTHENTICATE_ID
+RES= `sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub --json`
+echo ${RES}
+SFDC_AUTHENTICATE_ID=`echo ${RES} | jq --raw-output .result.orgId` 
+echo "OrgId..."
+echo ${SFDC_AUTHENTICATE_ID}
+
 
 #Create a scratch org
 echo "Name of Scratch Org"
 echo ${CIRCLE_BRANCH}
+
+failhere
+
 
 echo "Creating the Scratch Org..."
 sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s -d 1 --json 
