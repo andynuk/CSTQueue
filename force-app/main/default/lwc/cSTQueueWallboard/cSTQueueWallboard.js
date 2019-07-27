df49c323acf38cff6eed71923ddd38ac37d939b3
Andy Nix <andynuk@gmail.com>
2019-07-27 23:32:34 +0100

    wallboard

--- force-app/main/default/lwc/cSTQueueWallboard/cSTQueueWallboard.js
+++ force-app/main/default/lwc/cSTQueueWallboard/cSTQueueWallboard.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class CSTQueueWallboard extends LightningElement {}
+export default class CSTQueueWallboard extends LightningElement {
+
+    
+}
