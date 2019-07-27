ae91593430f62dc10bee397a0163569b72afa3f6
Andy Nix <andynuk@gmail.com>
2019-07-27 09:53:19 +0100

    tsting

--- build/install.sh
+++ build/install.sh
@@ -2,5 +2,4 @@
 echo "Installing Dependencies... "
 sudo npm install -global sfdx-cli
 echo "Installing plugins... "
-#sfdx plugins:install https://github.com/Accenture/sfpowerkit -f
-npm i sfpowerkit
+echo y |sfdx plugins:install https://github.com/Accenture/sfpowerkit -f
