ffe3039b8745bff9cad15dd5c19963afbd2dfff6
Andy Nix <andynuk@gmail.com>
2019-07-30 14:43:40 +0100

    check instance

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -9,7 +9,7 @@ echo $SFDC_SERVER_KEY | base64 -d > uatkeys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile uatkeys/server.key --username $SFDC_UAT_USER  -a UAT -r https://test.salesforce.com
+sfdx force:auth:jwt:grant --instanceurl https://test.salesforce.com --clientid $SFDC_PROD_CLIENTID --jwtkeyfile uatkeys/server.key --username $SFDC_UAT_USER  -a UAT 
  
 echo "New listing orgs we are authorised to..."
 sfdx force:auth:list
