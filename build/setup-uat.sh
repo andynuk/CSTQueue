b3d2c4182d40422f7f5cd52559a579780457c96a
Andy Nix <andynuk@gmail.com>
2019-07-30 14:17:08 +0100

    testing deploy UAT

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -1,7 +1,15 @@
 # Get the private key from the environment variable
 echo "Setting up UAT Connection..."
-mkdir keys
-echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
+mkdir uatkeys
+echo $SFDC_SERVER_KEY | base64 -d > uatkeys/server.key
+
+# check what we are authorised to connect to 
+echo "Listing orgs we are authorised to..."
+sfdx force:auth:list
+
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_UAT_USER --setdefaultdevhubusername -a UAT
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile uatkeys/server.key --username $SFDC_UAT_USER  -a UAT
+
+echo "New listing orgs we are authorised to..."
+sfdx force:auth:list
